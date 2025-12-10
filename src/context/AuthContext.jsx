38b9193bd41ac3e292170a3355ede5b9341db33d
ZUBAER <zubaer.developer@gmail.com>
2025-12-10 20:07:46 +0600

    firebase configeration setup

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { app } from "../firebase/firebase.config";
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD2pYkKHc70tAfQXzTFimzdSIJ9KIR9fB4",
+  authDomain: "game-hub-419f6.firebaseapp.com",
+  projectId: "game-hub-419f6",
+  storageBucket: "game-hub-419f6.firebasestorage.app",
+  messagingSenderId: "378822241265",
+  appId: "1:378822241265:web:d9a34806656354872bb250",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
